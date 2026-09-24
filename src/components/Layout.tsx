e5fcbb5fd3257aa792c4b0f82d5a0c1ac544df0b
MeghrajRavani <meghrajravani@gmail.com>
2026-09-24 23:58:39 +0530

    feat: Update Layout component

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -8,15 +8,15 @@ export default function Layout() {
     <div className="flex-1 flex flex-col min-h-0">
       <div className={cn('flex-1 flex min-h-0', terminalPanelOpen ? '' : '')}>
         <main className="flex-1 flex flex-col min-h-0">
-          <div className="flex-1 min-h-0 p-2 bg-white dark:bg-gray-900">
-            <div className="text-sm text-gray-500 dark:text-gray-400">Editor Area</div>
+          <div className="flex-1 min-h-0">
+            <MonacoEditor />
           </div>
         </main>
       </div>
       {terminalPanelOpen && (
         <div className="h-40 border-t border-gray-200 dark:border-gray-700 bg-gray-50 dark:bg-gray-800">
           <div className="h-full flex flex-col p-2">
-            <div className="text-sm text-gray-500 dark:text-gray-400">Terminal</div>
+            <Terminal />
           </div>
         </div>
       )}
